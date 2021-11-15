4b5fad3eb4c05332e42151d1ed727b4aadc08459
waffles <waffles@arbitraryexecution.com>
2021-11-15 00:45:12 -0500

    ran black on the file

--- convert_hex_to_bin.py
+++ convert_hex_to_bin.py
@@ -10,18 +10,18 @@ input_fpath = sys.argv[1]
 if not os.path.isfile(input_fpath):
     sys.exit(f"ERROR: {input_fpath} is not a valid file!")
 
-with open(input_fpath, 'r') as f:
+with open(input_fpath, "r") as f:
     data = f.read()
 
 
-if data[:2] == '0x':
+if data[:2] == "0x":
     data = data[2:]
 
 # Strip off any whitespace that may be at the end of the file
 data = data.rstrip()
 data_bytes = binascii.a2b_hex(data)
 
-with open(sys.argv[2], 'wb') as f:
+with open(sys.argv[2], "wb") as f:
     f.write(data_bytes)
 
-print(f'Finished creating file: {sys.argv[2]}')
+print(f"Finished creating file: {sys.argv[2]}")
