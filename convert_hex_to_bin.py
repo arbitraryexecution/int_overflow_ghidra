09346037f93acfb6bc0ef7a27f1fb22c61855c79
waffles <waffles@arbitraryexecution.com>
2021-11-15 00:43:52 -0500

    Added a comment to make the code a little cleaner

--- convert_hex_to_bin.py
+++ convert_hex_to_bin.py
@@ -13,10 +13,13 @@ if not os.path.isfile(input_fpath):
 with open(input_fpath, 'r') as f:
     data = f.read()
 
+
 if data[:2] == '0x':
     data = data[2:]
 
-data_bytes = binascii.a2b_hex(data.strip('\n'))
+# Strip off any whitespace that may be at the end of the file
+data = data.rstrip()
+data_bytes = binascii.a2b_hex(data)
 
 with open(sys.argv[2], 'wb') as f:
     f.write(data_bytes)
